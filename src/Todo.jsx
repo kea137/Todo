ae82911496edbdc12d78a868a8c7666dcbf48f1d
kea137 <kearajab@gmail.com>
2021-11-16 14:35:12 +0300

    Episode 2

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function Todo() {
+function Todo(props) {
     return (
         <div className="flex items-center ml-32">
-            <input className="ml-2" type="checkbox" />
-            <p className="ml-3 mr-28 my-4 text-lg">Important</p>
+            <input className="ml-2" type="checkbox" checked={props.isCompleted} />
+            <p className={`ml-3 mr-28 my-4 text-lg ${props.isCompleted ? "line-through" : ""}`}>{props.text}</p>
             <svg xmlns="http://www.w3.org/2000/svg" class=" h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
             </svg>
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import Todo from './Todo'
 
 function TodoList() {
+
+    const [todos, setTodos] = React.useState([
+        {id: 1, text: 'Learn React', isCompleted: false },
+        {id: 2, text: 'Learn Inertia', isCompleted: true },
+        {id: 3, text: 'Learn Laravel', isCompleted: false },
+    ]);
+
     return (
         <div className="content-center">
             <input type="text" className="rounded-md p-2 w-60 shadow-md" placeholder="What do you need to do?" />
-            <Todo/>
-            <Todo/>
-            <Todo/>
-            <Todo/>
+            {todos.map((todo, index)=>(
+            <Todo id={todo.id} text={todo.text} isCompleted={todo.isCompleted}/>
+            ))}
             <hr  className="mx-32 my-4"/>
             <div className=" justify-between flex ml-32 text-sm">
                 <button className="ml-1 rounded-md border border-gray-200 p-1">Check All</button>
